f8efd0a02d3b12c4d2e5ad4f602b5ebc52a05463
Naomi Macias <naomiperla.macias@hotmail.com>
2021-04-23 01:49:44 -0500

    V1 - Title page

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ const Layout = ({children}) => {
     return (
         <>
             <Head>
-                <title>SQMS Curso</title>
+                <title>Spreadgreg coaching</title>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg==" crossOrigin="anonymous" />
                 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"/>
             </Head>
