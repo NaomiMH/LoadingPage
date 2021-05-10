1b28f4513bc9c3ea3b10c61b5d1619fd8c95f231
Naomi Macias <naomiperla.macias@hotmail.com>
2021-05-10 13:55:16 -0500

    V10 - first boton fix

--- components/FrontPage.js
+++ components/FrontPage.js
@@ -30,18 +30,18 @@ export default function FrontPage() {
         </div>
         <div>
             {/* <!-- Begin Mailchimp Signup Form --> */}
-            <div id="mc_embed_signup">
-                <form action="https://gmail.us1.list-manage.com/subscribe/post?u=25e4b92583f7fb818436b0d16&amp;id=ac91c13f3f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" className={styles.endDiv} target="_blank" novalidate>
+            <div id="mc_embed_signup" className={styles.endDiv}>
+                <form action="https://gmail.us1.list-manage.com/subscribe/post?u=25e4b92583f7fb818436b0d16&amp;id=ac91c13f3f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                     <div id="mc_embed_signup_scroll" className={styles.form}>
-                        <div class="mc-field-group" className={styles.mail2}>
-                            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" className={styles.email} />
+                        <div class="mc-field-group" id={styles.mail2}>
+                            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" />
                         </div>
                         <div id="mce-responses" class="clear">
                             <div class="response" id="mce-error-response" className={styles.hide}></div>
                             <div class="response" id="mce-success-response" className={styles.hide}></div>
                         </div>    {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
                         <div className={styles.mail1} aria-hidden="true"><input type="text" name="b_25e4b92583f7fb818436b0d16_ac91c13f3f" tabindex="-1" value="" /></div>
-                        <div class="clear"  className={styles.mail3}><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" className={styles.btnemail} /></div>
+                        <div class="clear" id={styles.mail3}><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" /></div>
                     </div>
                 </form>
             </div>
