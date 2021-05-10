1a4b81077ef3ce7a1438f77a2ee7c58bed528585
Naomi Macias <naomiperla.macias@hotmail.com>
2021-05-10 14:26:48 -0500

    V9 - reset

--- components/FrontPage.js
+++ components/FrontPage.js
@@ -28,28 +28,11 @@ export default function FrontPage() {
         <div className={styles.middleDiv}>
             <p className={styles.middleP}>Únete a mi lista y no te pierdas las novedades acerca de mis programas de coaching para traders, podcasts y webinars</p>
         </div>
-        <div>
-            {/* <!-- Begin Mailchimp Signup Form --> */}
-            <div id="mc_embed_signup" className={styles.endDiv}>
-                <form action="https://gmail.us1.list-manage.com/subscribe/post?u=25e4b92583f7fb818436b0d16&amp;id=ac91c13f3f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                    <div id="mc_embed_signup_scroll" className={styles.form}>
-                        <div class="mc-field-group" id={styles.mail2}>
-                            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" />
-                        </div>
-                        <div id="mce-responses" class="clear">
-                            <div class="response" id="mce-error-response" className={styles.hide}></div>
-                            <div class="response" id="mce-success-response" className={styles.hide}></div>
-                        </div>    {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
-                        <div className={styles.mail1} aria-hidden="true"><input type="text" name="b_25e4b92583f7fb818436b0d16_ac91c13f3f" tabindex="-1" value="" /></div>
-                        <div class="clear" id={styles.mail3}><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" /></div>
-                    </div>
-                </form>
-            </div>
-            {/* <!--End mc_embed_signup--> */}
-            {/* <form className={styles.form}>
+        <div className={styles.endDiv}>
+            <form className={styles.form}>
             <input type="email" className={styles.email}/>
             <input type="button" value="QUIERO APRENDER" className={styles.btnemail}/>
-            </form> */}
+            </form>
         </div>
     </div>
   )
